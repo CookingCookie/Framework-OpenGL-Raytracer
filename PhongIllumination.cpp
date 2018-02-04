2c7cd3df979b90199ae23c30c2e9af199a46af8f
Philip Koslowski <philip.koslowski@gmail.com>
2018-02-04 16:45:14 +0100

    etwas code gesäubert

--- PhongIllumination.cpp
+++ PhongIllumination.cpp
@@ -10,34 +10,6 @@ PhongIllumination::~PhongIllumination() {
 
 }
 
-Vec3f PhongIllumination::IlluminationCalculation_correct_Phong(SceneObject& sceneObject, vector <light>& lightSources, Vec3f hitPoint, Vec3f N, Vec3f V) {
-	unsigned int lss = lightSources.size();
-	Vec3f I_ambient, DS_sumOverLights, L, LV, H;
-	// iterate over r,g,b
-	for (unsigned int i = 0; i < 3; i++) {
-		I_ambient[i] = environmentalLightIntensity[i] * sceneObject.matAmbient[i];
-		// iterate over all light sources
-		for (unsigned int l = 0; l < lss; l++) {
-			L = lightSources[l].lightPos - hitPoint;
-			L.normalize();
-			LV = L + V;
-			H = (LV) / (helper.eukl(LV));
-			float KdFd = sceneObject.matDiffuse[i] * (L * N);
-			float KsFs = sceneObject.matSpecular[i] * (float)pow(Ke, (H * N));
-			DS_sumOverLights[i] += lightSources[l].lightIntensity * (KdFd + KsFs);
-		}
-	}
-	return I_ambient + DS_sumOverLights;
-}
-
-Vec3f PhongIllumination::CalcAmbientTerm(SceneObject& sceneObject) {
-	Vec3f I_ambient;
-	for (unsigned int i = 0; i < 3; i++) {
-		I_ambient[i] = environmentalLightIntensity[i] * sceneObject.matAmbient[i];
-	}
-	return I_ambient;
-}
-
 Vec3f PhongIllumination::IlluminationCalculation(SceneObject& sceneObject, vector <light>& lightSources,
 	Vec3f hitPoint, Vec3f N, Vec3f V, vector <unsigned int> S) {
 	unsigned int lss = lightSources.size();
--- PhongIllumination.h
+++ PhongIllumination.h
@@ -19,10 +19,6 @@ public:
 	Vec3f IlluminationCalculation(SceneObject& sceneObject, vector <light>& lightSources,
 		Vec3f hitPoint, Vec3f N, Vec3f V, vector <unsigned int> S);
 
-	Vec3f CalcAmbientTerm(SceneObject& sceneObject);
-
-	Vec3f IlluminationCalculation_correct_Phong(SceneObject& sceneObject, vector <light>& lightSources, Vec3f hitPoint, Vec3f N, Vec3f V);
-
 private:
 	// mirror coefficient 
 	unsigned int Ke = 64;
